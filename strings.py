ef59c415a9440ca0e1e0cd06f91fd56291e61eb7
padams73 <pad@stac.school.nz>
2022-03-04 10:48:28 +1300

    isalpha and printing in columns

---
+++ strings.py
@@ -0,0 +1,10 @@
+'''name = "R2-D2"
+
+if name.isalpha():
+    print("All characters are from the alphabet")
+else:
+    print("Not all alphabet characters")
+'''
+name = "John"
+country = "New Zealand"
+print(f"{name :10} {country :12}")
