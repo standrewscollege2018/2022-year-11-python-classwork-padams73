27a9e1c371e81ec9e0cfa882da46718f1a4c916f
padams73 <pad@stac.school.nz>
2022-05-19 18:05:41 +1200

    Revert "Removed commented code"
    This reverts commit feadf9ce2200aefae1726f33d39cef3caddd22cf.

--- cars-db.py
+++ cars-db.py
@@ -11,7 +11,12 @@ cursor = connection.cursor()
 # Query 1: Select all info from the car table
 results = cursor.execute("SELECT * FROM car")
 
-
+# Display cars down the page
+print(f"{'ID':3} {'Plate':6} {'Colour':10} {'Driver':15} {'Make':10} {'Model':12}")
+print("="*56)
+for result in results:
+    print(f"{result[0]:3} {result[1]:6} {result[2]:10} {result[3]:15} {result[4]:10} {result[5]:12}")
+'''
 print("Welcome to the cars database")
 ask = True
 while ask:
@@ -21,12 +26,9 @@ while ask:
     else:
         ask = False
     
-        
-query = f"SELECT * FROM car WHERE number_plate LIKE '%{plate}%'"
-print(query)
-cursor.execute(query)
+plate = "%"+plate+"%"
+cursor.execute("SELECT * FROM car WHERE number_plate LIKE ?", (plate,))
 results = cursor.fetchall()
-
 #print(f"{len(results)} result(s) found")
 if len(results)>0:
     print(f"{'ID':3} {'Plate':6} {'Colour':10} {'Driver':15} {'Make':10} {'Model':12}")
